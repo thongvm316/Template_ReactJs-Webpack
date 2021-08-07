22f6f19abfa5110ddd546441f92806a84572e901
BM James <thongvm@brickmate.kr>
2021-08-07 10:59:25 +0700

    inital webpack setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import { render } from "react-dom";
+
+// import App from "./components/App";
+
+// render(<App />, document.getElementById("root"));
+
+const forTest = {
+  a: 'fd',
+  b: 'fdfdf',
+  c: 2323,
+}
+
+console.log(forTest)
